0b0bbce9ad541dcd3f5de040457f7db594fc2071
kennytobi <kennytobi@github.com>
2023-04-26 15:35:32 -0700

    simple shell project

--- builtin.c
+++ builtin.c
@@ -15,7 +15,7 @@ int _myexit(info_t *info)
 		if (exitcheck == -1)
 		{
 			info->status = 2;
-			print_error(info, "illegal number: ");
+			print_error(info, "Illegal number: ");
 			_eputs(info->argv[1]);
 			_eputchar('\n');
 			return (1);
@@ -42,7 +42,7 @@ int _mycd(info_t *info)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-		dir = _getenv(info, "Home=");
+		dir = _getenv(info, "HOME=");
 		if (!dir)
 			chdir_ret =
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
